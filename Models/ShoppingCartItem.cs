4eb67c4d9e55efd71ca1ca365ad0ecbf223adbdb
Anıl Pak <71243066+anLpk@users.noreply.github.com>
2021-07-21 14:01:42 +1000

    navigation added - shopping cart model and cotroller created

---
+++ Models/ShoppingCartItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class ShoppingCartItem
+    {
+        public int ShoppingCartitemId { get; set; }
+        public Pie Pie { get; set; }
+        public int Amount { get; set; }
+        public string ShoppingCartId { get; set; }
+
+    }
+}
